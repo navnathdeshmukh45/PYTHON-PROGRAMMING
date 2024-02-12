4cdd96232093096d0baf93fa8f4084501254e481
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2024-02-12 20:56:28 +0530

    DSA

---
+++ DSA WITH PYTHON/Basic python.py
@@ -0,0 +1,96 @@
+print("Print Simple Statement")
+# variable
+var = 45
+print(var)
+
+# Operators
+# program arithmatic operator
+# a=45
+# b=34
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# program on relational op
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+#bitwise op
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# print(~b)
+# print(a<<b)
+# print(a>>b)
+
+#assignment op
+# x=10
+# x+=0
+# print(x)
+# x-=20
+# print(x)
+# x*=10
+# print(x)
+# x/=10
+# print(x)
+
+#ternary op
+# y= input("Enter the value y")
+# z=input("Enter the value z")
+# min =y if y<z else z
+# print("minmum  value :",min)
+
+# String 
+string = "This is string"
+print(string)
+
+# list
+list =[1,2,3,4,5]
+print(list)
+
+# dicinary
+dict ={"MATH":45,"CHE":56}
+print(dict)
+
+# set 
+set ={1,2,3,4,5}
+print(set)
+
+# tuple
+tuple =(1,2,3,4,5)
+print(tuple)
+
+# function
+def sum(a,b):
+  {
+    print(4,5)
+  }
+sum(3,6)
+
+# condition 
+# let = (input("Enter The Name :"))
+# if(let == "ram"):
+#  {
+#   print("Hello Sir, How Are You ?")
+# }
+# else:
+#   {
+#     print("Good Moring Guest")
+#   }
+
+#loops 
+# for a in range(1,4):
+#  print(a)
+
+# for i in range(7):
+#     print(i)
+#     i=i+1
+#     continue
+#     break
+
+i=1
+while(i<=5):
+    print(i)
+    i=i+1
---
+++ DSA WITH PYTHON/check number is positive or negitive.py
@@ -0,0 +1,7 @@
+a =int(input("Enter the number is "))
+if a >0:
+    print("number is positive")
+elif a<0:
+    print("Number is negitive")
+else:
+    print("A is 0")
